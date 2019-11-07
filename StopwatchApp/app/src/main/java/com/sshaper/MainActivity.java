7d2b75ea10e1ebd1c4addb3fae3aeb2755c86f3d
sshaper <sshaper@sshaper.com>
2019-11-07 15:14:00 -0500

    add stopwatch application

---
+++ StopwatchApp/app/src/main/java/com/sshaper/MainActivity.java
@@ -0,0 +1,61 @@
+package com.sshaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimmer();
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimmer(){
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = (seconds%60);
+
+                String time = String.format(Locale.getDefault(),"%d:%02d:%02d", hours, minutes, secs);
+
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+
+        });
+
+    }
+
+
+}
