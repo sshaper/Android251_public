cc0e0d54ff1dd60649047ab9891a2d5d28b05e05
sshaper <sshaper@sshaper.com>
2020-03-03 22:45:22 -0500

    updates

---
+++ BookExamples/45_TabLayoutDemoV2/app/src/main/java/com/ebookfrenzy/tablayoutdemov2/TabPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.ebookfrenzy.tablayoutdemov2;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabPagerAdapter extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.tabCount = numberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+
+            default:
+                return null;
+        }
+    }
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
