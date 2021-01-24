7b724fe5f0430668e68612b674305941c8ec0075
sshaper <sshaper@sshaper.com>
2021-01-24 09:38:11 -0500

    updates

--- ScottsExamples/DataBinding/app/src/main/java/com/example/databinding/ui/main/MainViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.example.databinding.ui.main;
-
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    public String result = "Scott";
-    // TODO: Implement the ViewModel
-}
