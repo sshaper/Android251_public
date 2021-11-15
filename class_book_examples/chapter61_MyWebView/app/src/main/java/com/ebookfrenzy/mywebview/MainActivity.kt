6417dd6e8e6191e14648ec0c5a3677c061c8d738
sshaper <sshaper@sshaper.com>
2021-11-15 10:21:21 -0500

    updates

---
+++ class_book_examples/chapter61_MyWebView/app/src/main/java/com/ebookfrenzy/mywebview/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ebookfrenzy.mywebview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import java.net.URL
+
+import com.ebookfrenzy.mywebview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+
+        val intent = this.intent
+        val data = intent.data
+        var url: URL? = null
+
+        try {
+            url = URL(data?.scheme,
+                data?.host,
+                data?.path)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        binding.webView1.loadUrl(url.toString())
+    }
+
+}
