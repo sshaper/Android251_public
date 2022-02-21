8785e206f9700854f87355523dbde4fe0ee8a261
sshaper <sshaper@sshaper.com>
2022-02-21 08:38:49 -0500

    updates

---
+++ scotts_head_first_examples/My_constraint_layout/app/src/main/java/com/scottshaper/my_constraint_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.scottshaper.my_constraint_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
