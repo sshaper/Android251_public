addc4bdb42e3ee93027a6d0b26d843ffc4c30f3f
sshaper <sshaper@sshaper.com>
2022-03-22 17:54:13 -0400

    updates

--- MultiFragmentNavigation/app/src/main/java/com/example/multifragmentnavigation/MainActivity.kt
+++ MultiFragmentNavigation/app/src/main/java/com/example/multifragmentnavigation/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.multifragmentnavigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.multifragmentnavigation.ui.main.MainFragment
+import android.net.Uri
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
     }
+
+    //override fun onFragmentInteraction(uri: Uri){
+
+    //}
 }
--- MultiFragmentNavigation/app/src/main/java/com/example/multifragmentnavigation/ui/main/MainFragment.kt
+++ MultiFragmentNavigation/app/src/main/java/com/example/multifragmentnavigation/ui/main/MainFragment.kt
@@ -7,8 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.multifragmentnavigation.R
+import com.example.multifragmentnavigation.databinding.MainFragmentBinding
+import androidx.navigation.Navigation
 
 class MainFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = MainFragment()
@@ -16,17 +21,36 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        savedInstanceState: Bundle?): View {
+        //return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.button1.setOnClickListener {
+            //Navigation.findNavController(it).navigate(
+                //R.id.action_mainFragment_to_secondFragment
+            //)
+            val action1: MainFragmentDirections.ActionMainFragmentToSecondFragment = MainFragmentDirections.actionMainFragmentToSecondFragment()
+            action1.setMessage1(binding.name.text.toString())
+            Navigation.findNavController(it).navigate(action1)
+        }
+
+        binding.button2.setOnClickListener {
+            val action2: MainFragmentDirections.ActionMainFragmentToThirdFragment = MainFragmentDirections.actionMainFragmentToThirdFragment()
+            action2.setMessage1(binding.name.text.toString())
+            Navigation.findNavController(it).navigate(action2)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
     }
 
 }
