53fd2e2c43387a69e58dd9d3907c646431e71f39
sshaper <sshaper@sshaper.com>
2022-04-01 07:46:02 -0400

    updates

---
+++ students/cris_/cris_castro_card_project/app/src/main/java/com/example/recycleviewproject/ui/main/RecyclerAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.recycleviewproject.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleviewproject.R
+import com.google.android.material.snackbar.Snackbar
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private val titles = arrayOf("Chapter One",
+        "Chapter Two", "Chapter Three", "Chapter Four",
+        "Chapter Five", "Chapter Six", "Chapter Seven",
+        "Chapter Eight")
+
+    private val details = arrayOf("Item one details", "Item two details",
+        "Item three details", "Item four details",
+        "Item five details", "Item six details",
+        "Item seven details", "Item eight details")
+
+    private val images = intArrayOf(
+        R.drawable.android_image_1,
+        R.drawable.android_image_2, R.drawable.android_image_3,
+        R.drawable.android_image_4, R.drawable.android_image_5,
+        R.drawable.android_image_6, R.drawable.android_image_7,
+        R.drawable.android_image_8)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemDetail.text = details[i]
+        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.itemImage)
+            itemTitle = itemView.findViewById(R.id.itemTitle)
+            itemDetail = itemView.findViewById(R.id.itemDetail)
+
+            itemView.setOnClickListener { v: View ->
+                var position: Int = getAdapterPosition()
+
+                Snackbar.make(v, "Click detected on item $position",
+                    Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            }
+        }
+    }
+}
