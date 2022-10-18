b54e06be11cb6569abd99c56e2d0f4c5c21e83f1
sshaper <sshaper@sshaper.com>
2022-10-18 10:17:19 -0400

    updates

--- scotts_kotlin_examples/DolphinTest/app/src/main/java/com/scottshaper/dolphintest/ui/main/MainFragment.kt
+++ scotts_kotlin_examples/DolphinTest/app/src/main/java/com/scottshaper/dolphintest/ui/main/MainFragment.kt
@@ -4,16 +4,19 @@ import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.lifecycle.Observer
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.scottshaper.dolphintest.R
 
 import com.scottshaper.dolphintest.databinding.FragmentMainBinding
+import com.scottshaper.dolphintest.BR.myViewModel
 
 class MainFragment : Fragment() {
 
-    private var _binding: FragmentMainBinding? = null
-    private val binding get() = _binding!!
+    //private var _binding: FragmentMainBinding? = null
+    //private val binding get() = _binding!!
 
 
     companion object {
@@ -21,6 +24,7 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    lateinit var binding: FragmentMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,31 +37,44 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         //return inflater.inflate(R.layout.fragment_main, container, false)
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        //_binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_main, container, false)
+        binding.setLifecycleOwner(this)
         return binding.root
     }
 
 
-    //ONACTIVITYCREATED IS DEPRECATED INSTEAD ONVIEWCREATED IS USED.
+    //ONACTIVITYCREATED IS DEPRECATED INSTEAD ONVIEWCREATED IS USED. HOWEVER IN THE DATABINDING YOU JUST NEED THE BINDING.SETVARIABLE... LINE
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.resultText.text = viewModel.getResult().toString()
+        binding.setVariable(myViewModel, viewModel)
+        //binding.resultText.text = viewModel.getResult().toString()
+
+        //THIS IS REMOVED BECAUSE WE REPLACE IT WITH DATABINDING
+/*
+        val resultObserver = Observer<Float> {
+                result -> binding.resultText.text = result.toString()
+        }
+
+        viewModel.getResult().observe(viewLifecycleOwner,resultObserver)
 
         binding.convertButton.setOnClickListener {
             if (binding.dollarText.text.isNotEmpty()) {
                 viewModel.setAmount(binding.dollarText.text.toString())
-                binding.resultText.text = viewModel.getResult().toString()
+                //binding.resultText.text = viewModel.getResult().toString()
             } else {
                 binding.resultText.text = "No Value"
             }
-        }
+        }*/
     }
 
-    override fun onDestroyView() {
+    /*override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
+    }*/
 
 
 
--- scotts_kotlin_examples/DolphinTest/app/src/main/java/com/scottshaper/dolphintest/ui/main/MainViewModel.kt
+++ scotts_kotlin_examples/DolphinTest/app/src/main/java/com/scottshaper/dolphintest/ui/main/MainViewModel.kt
@@ -1,20 +1,39 @@
 package com.scottshaper.dolphintest.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+
 
 class MainViewModel : ViewModel() {
     private val rate = 0.74f
-    private var dollarText = ""
-    private var result: Float = 0f
+    //private var dollarText = ""
+    //private var result: Float = 0f
+    //private var result: MutableLiveData<Float> = MutableLiveData()
+
+    var dollarValue: MutableLiveData<String> = MutableLiveData()
+    var result: MutableLiveData<Float> = MutableLiveData()
+
+    fun convertValue() {
+        dollarValue.let {
+            if (!it.value.equals("")) {
+                result.value = it.value?.toFloat()?.times(rate)
+            } else {
+                result.value = 0f
+            }
+        }
+    }
 
 
-    fun setAmount(value: String) {
+
+
+    /*fun setAmount(value: String) {
         this.dollarText = value
-        result = value.toFloat() * rate
+        //result = value.toFloat() * rate
+        result.setValue(value.toFloat() * rate)
     }
 
 
-    fun getResult(): Float {
+    fun getResult(): MutableLiveData<Float> {
         return result
-    }
+    }*/
 }
