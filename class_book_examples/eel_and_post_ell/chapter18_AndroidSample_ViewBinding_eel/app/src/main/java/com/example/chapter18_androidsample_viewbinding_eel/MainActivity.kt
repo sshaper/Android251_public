8ebdea894779dcf2b6940361b1640f485a758654
sshaper <sshaper@sshaper.com>
2023-01-18 11:50:32 -0500

    updates

---
+++ class_book_examples/eel_and_post_ell/chapter18_AndroidSample_ViewBinding_eel/app/src/main/java/com/example/chapter18_androidsample_viewbinding_eel/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.chapter18_androidsample_viewbinding_eel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+import com.example.chapter18_androidsample_viewbinding_eel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun convertCurrency(view: View) {
+
+        if (binding.dollarText.text.isNotEmpty()) {
+
+            val dollarValue = binding.dollarText.text.toString().toFloat()
+
+            val euroValue = dollarValue * 0.85f
+
+            binding.textView.text = euroValue.toString()
+        } else {
+            binding.textView.text = getString(R.string.no_value_string)
+        }
+    }
+}
