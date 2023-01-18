c4c4fe9125ca4313d0a5126759840991e9c4f3c8
sshaper <sshaper@sshaper.com>
2023-01-18 16:46:41 -0500

    updates

---
+++ class_book_examples/eel_and_post_ell/chapter33_EventExample_eel/app/src/main/java/com/example/chapter33_eventexample_eel/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.chapter33_eventexample_eel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+import com.example.chapter33_eventexample_eel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.myButton.setOnClickListener {
+            binding.statusText.text = "Button click"
+        }
+
+        binding.myButton.setOnLongClickListener {
+            binding.statusText.text = "Long button click"
+            false
+        }
+    }
+}
