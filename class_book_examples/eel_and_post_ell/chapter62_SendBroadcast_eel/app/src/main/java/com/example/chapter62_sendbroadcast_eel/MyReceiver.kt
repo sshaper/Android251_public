6289092252fccd9bd9ed1fcf886eef0a3b43f6ce
sshaper <sshaper@sshaper.com>
2023-02-22 12:43:40 -0500

    updates

---
+++ class_book_examples/eel_and_post_ell/chapter62_SendBroadcast_eel/app/src/main/java/com/example/chapter62_sendbroadcast_eel/MyReceiver.kt
@@ -0,0 +1,28 @@
+package com.example.chapter62_sendbroadcast_eel
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val extras = intent.extras?:return
+        var name = ""
+        extras.getString("name").let {
+            if (it != null) {
+                name = it
+            }
+        }
+
+        var message = "Broadcast intent detected " + intent.action + "  " + name
+        //I DID THIS TO DETERMINE WHICH MESSAGE TO FIRE DEPENDING ON THE ACTION
+        //BASICALLY I JUST CHANGE THE MESSAGE.
+        if(intent.action == "android.intent.action.ACTION_POWER_DISCONNECTED"){
+            message = "Power has been disconnected"
+        }
+        Toast.makeText(context, message,Toast.LENGTH_LONG).show()
+    }
+}
