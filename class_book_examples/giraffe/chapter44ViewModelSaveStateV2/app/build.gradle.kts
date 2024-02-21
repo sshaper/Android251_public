f8f74e2e46fae281e83cfc69cb12141af9266726
sshaper <sshaper@sshaper.com>
2024-02-21 16:47:21 -0500

    updates

--- class_book_examples/giraffe/chapter44ViewModelSaveStateV2/app/build.gradle.kts
+++ class_book_examples/giraffe/chapter44ViewModelSaveStateV2/app/build.gradle.kts
@@ -6,14 +6,15 @@ plugins {
 android {
 
     buildFeatures{
-        viewBinding = true
+        viewBinding=true
     }
 
-    namespace = "com.example.chapter44ViewModelSavedState"
+
+    namespace = "com.example.chapter44viewmodelsavestatev2"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.chapter44ViewModelSavedState"
+        applicationId = "com.example.chapter44viewmodelsavestatev2"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -38,22 +39,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
     implementation ("androidx.savedstate:savedstate:1.2.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- class_book_examples/giraffe/chapter44ViewModelSaveStateV2/app/src/main/java/com/example/chapter44viewmodelsavestatev2/MainActivity.kt
+++ class_book_examples/giraffe/chapter44ViewModelSaveStateV2/app/src/main/java/com/example/chapter44viewmodelsavestatev2/MainActivity.kt
@@ -1,9 +1,12 @@
-package com.example.chapter40viewmodeldemo_basic
+package com.example.chapter44viewmodelsavestatev2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.chapter40viewmodeldemo_basic.databinding.ActivityMainBinding
+import androidx.lifecycle.SavedStateViewModelFactory
+import com.example.chapter44viewmodelsavestatev2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,17 +15,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        this?.application?.let {
+            val factory = SavedStateViewModelFactory(it, this)
+
+            viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+        }
+
+
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        binding.resultText.text = viewModel.getResult().toString()
+        //binding.resultText.text = viewModel.getResult().toString()
+
+
+
+
+        val resultObserver = Observer<Float>{
+            result -> binding.resultText.text = result.toString()
+        }
+
+        viewModel.getResult().observe(this,resultObserver)
+
 
         binding.convertButton.setOnClickListener{
             if (binding.dollarText.text.isNotEmpty()) {
                 viewModel.setAmount(binding.dollarText.text.toString())
-                binding.resultText.text = viewModel.getResult().toString()
+                //binding.resultText.text = viewModel.getResult().toString()
             } else {
                 binding.resultText.text = "No Value"
             }
