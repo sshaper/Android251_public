e661e8f8a208fc0ecc70a0ea92e0f2c2d886b1fb
sshaper <sshaper@sshaper.com>
2024-05-04 15:38:04 -0400

    updates

---
+++ NewCPS251Examples/chapter8/RecycleView/app/src/main/java/com/example/recycleview/Data.kt
@@ -0,0 +1,19 @@
+package com.example.recycleview
+
+class Data {
+    public val titles = arrayOf("Chapter One",
+        "Chapter Two", "Chapter Three", "Chapter Four",
+        "Chapter Five", "Chapter Six", "Chapter Seven",
+        "Chapter Eight")
+
+    public val details = arrayOf("Item one details", "Item two details",
+        "Item three details", "Item four details",
+        "Item five details", "Item six details",
+        "Item seven details", "Item eight details")
+
+    public val images = intArrayOf(R.drawable.android_image_1,
+        R.drawable.android_image_2, R.drawable.android_image_3,
+        R.drawable.android_image_4, R.drawable.android_image_5,
+        R.drawable.android_image_6, R.drawable.android_image_7,
+        R.drawable.android_image_8)
+}
---
+++ NewCPS251Examples/chapter8/RecycleView/app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.recycleview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.recycleview.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        val data = Data() // Your data class instance
+        adapter = RecyclerAdapter(data)
+        binding.recyclerView.adapter = adapter
+    }
+}
